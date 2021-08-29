c4c76d6535611b3d26abb7d9e66543a53a952010
aninarafath6 <arafathanin6.com>
2021-08-29 15:33:22 +0530

    Let's build uber clone

--- lib/views/pages/home/home_page.dart
+++ lib/views/pages/home/home_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Center(
+      child: Text("Let's Build uber clone"),
+    ));
   }
 }
